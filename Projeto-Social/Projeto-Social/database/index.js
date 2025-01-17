f0626bb4086685e164808516f3a99b1da1d3b1d1
vinialg <123613916+vinialg@users.noreply.github.com>
2025-01-17 02:22:37 -0300

    Update index.js

--- Projeto-Social/Projeto-Social/database/index.js
+++ Projeto-Social/Projeto-Social/database/index.js
@@ -2,15 +2,12 @@
 // const { join } = require ("node:path")
 
 // const db = pgp("postgres://postgres:password@localhost:5432/infousuario");
-
-// Configuração de conexão com o banco de dados
 const pgp = require('pg-promise')();
 const db = pgp({
-    host: 'dpg-cu4odl1u0jms73edgnu0-a.oregon-postgres.render.com',
-    port: 5432,
-    database: 'infousuario',
-    user: 'seila',
-    password: 'l1M0YJ6JikHTMk6Lvd7rsvwUjTMSNrZz'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 // db.query("SELECT 1 + 1 AS result").then((result) => console.log(result))
